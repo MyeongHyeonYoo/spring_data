ba61a6cbb152f6b5a973c058eb76820e33e5a2eb
MyeongHyeonYoo <victorea5@naver.com>
2023-01-02 18:08:39 +0900

    spr_data updata

--- ex00/src/test/java/org/zerock/persistence/DataSourceTests.java
+++ ex00/src/test/java/org/zerock/persistence/DataSourceTests.java
@@ -6,6 +6,8 @@ import java.sql.Connection;
 
 import javax.sql.DataSource;
 
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,6 +23,21 @@ public class DataSourceTests {
 	
 	@Autowired
 	private DataSource dataSource;
+	
+	@Autowired
+	private SqlSessionFactory sqlSessionFactory;
+	
+	@Test
+	public void testMyBatis() {
+		try (SqlSession session = sqlSessionFactory.openSession();
+			Connection con = session.getConnection();	
+			) {
+			log.info(session);
+			log.info(con);
+		} catch (Exception e) {
+			fail(e.getMessage());
+		}
+	}
 	
 	@Test
 	public void testConnction() {
